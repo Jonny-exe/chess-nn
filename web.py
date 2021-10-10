27f0a7d5d5d07c766db4db088efa8991015ada99
Jonny-exe <69385502+Jonny-exe@users.noreply.github.com>
2021-10-10 15:05:27 +0200

    added rest button

--- web.py
+++ web.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python3
-from flask import Flask, request
+from flask import Flask, request, redirect
 import chess
 import chess.svg
 from game import Game
 
-game = Game(None, False)
 app = Flask(__name__)
-print(game)
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def hello_world():
+    if 'game' not in globals():
+        global game
+        game = Game(None, False)
+
+    if request.args.get('reset', default=0, type=int) == 1:
+        game = Game(None, False)
+
     svg = game.get_svg()
     game.play_best_move()
-    print("HellO")
     return f"<html><p><img width=600 height=600 src='data:image/svg+xml;base64,{svg}'></img></p><br/>\
             <form action='/'><button type=submit>Hello</button></form> \
+            <form action='/?reset=1'><input type=hidden name=reset value=1></input> <button type=submit>reset</button></form> \
             </html>"
 
 if __name__ == "__main__":
